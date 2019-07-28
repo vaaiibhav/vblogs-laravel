9def4f39f082e59dd89bbbce34a0ccf650ecd07b
vaaiibhav <vaaiibhav@live.com>
2019-07-28 22:08:14 +0530

    added more views

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hi', function () {
+    return "hi vaaiibhav";
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
